318ff43f2d6b815e1c8eca5d7e4d737bf434592e
Rawaa Baessa <rawaabaessa@gmail.com>
2026-07-02 15:05:23 +0300

    refactor header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
-import Nav from "./Nav";
-import logoDark from "../assets/logo-dark.svg";
-import logoLight from "../assets/logo-light.svg";
-import { useTheme } from "../contexts/ThemeContext";
+// import Nav from "./Nav";
+// import logoDark from "../assets/logo-dark.svg";
+// import logoLight from "../assets/logo-light.svg";
+// import { useTheme } from "../contexts/ThemeContext";
 
 const Header = () => {
-  const { theme } = useTheme();
+  // const { theme } = useTheme();
   return (
     <>
-      <header className="sticky top-0 w-full z-50">
+      <header>
+        <h1>header</h1>
+      </header>
+      {/* <header className="sticky top-0 w-full z-50">
         <div className="flex items-center justify-between px-6 py-6 lg:px-32">
           {theme === "dark" ? (
             <img src={logoDark} className="w-10 md:w-16" />
@@ -16,7 +19,7 @@ const Header = () => {
           )}
           <Nav />
         </div>
-      </header>
+      </header> */}
     </>
   );
 };
